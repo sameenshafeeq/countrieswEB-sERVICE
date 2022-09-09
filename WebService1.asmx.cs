b7db3924a0868038fd1d67b19f759b08d2b399d5
Sameen Shafeeq <sameenshafeeq123@gmail.com>
2022-09-09 06:42:06 -0400

    Add files via upload

---
+++ WebService1.asmx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace ServiceDataApp
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public string insertEmpoyees(int id,string lastname, string firstname, string address , int Salary)
+        {
+            _myBuisnessAcessLayer b1 = new _myBuisnessAcessLayer
+            {
+                EmployeeID = id,
+                FirstName = firstname,
+                LastName = lastname,
+                Address = address,
+                Salary = Salary
+            };
+
+            string status = b1.Insert();
+            if (status == "PASS")
+                return "Employee details inserted!!";
+            else
+                return "Employees details cannot be inserted!";
+        }
+    }
+}
---
+++ _myBuisnessAcessLayer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ServiceDataApp
+{
+    public class _myBuisnessAcessLayer
+    {
+        public int  _EmployeeId;
+        public string _EmployeeLastName;
+        public string _EmployeeFirstName;
+        public string _Address;
+        public int _Salary;
+        public int EmployeeID
+        {
+            get { return _EmployeeId; }
+            set { _EmployeeId = value;  }
+        }
+
+        public string FirstName
+        {
+            get { return _EmployeeFirstName; }
+            set { _EmployeeFirstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _EmployeeLastName; }
+            set {  _EmployeeLastName = value; }
+        }
+
+        public string Address
+        {
+            get { return _Address; }
+            set { _Address = value; }
+        }
+
+        public int Salary
+        {
+            get { return _Salary; }
+            set { _Salary = value; }
+        }
+        public _myBuisnessAcessLayer() { }
+        public string Insert()
+        {
+            string con = ConfigurationManager.ConnectionStrings["Dbcon"].ConnectionString;
+            using (SqlConnection sqlconn = new SqlConnection(con))
+            {
+                SqlCommand sqlcmd = new SqlCommand("spSyapa", sqlconn);
+                sqlcmd.CommandType = CommandType.StoredProcedure;
+                sqlcmd.Parameters.AddWithValue("@id", EmployeeID);
+                sqlcmd.Parameters.AddWithValue("@last", LastName);
+                sqlcmd.Parameters.AddWithValue("@first", FirstName);
+                sqlcmd.Parameters.AddWithValue("@address", Address);
+                sqlcmd.Parameters.AddWithValue("@salary", Salary);
+                sqlconn.Open();
+                int check = sqlcmd.ExecuteNonQuery();
+                if (check > 0)
+                    return "PASS";
+                else
+                    return "FAIL";
+            }
+
+        }
+
+    }
+}
